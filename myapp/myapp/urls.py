7eb8399023cf3110770e1ccab4823c588011bbba
Enrique Nuñez <enunez@informat.cl>
2017-09-29 21:57:13 +0000

    Creación de mi primera vista y modelos

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from polls import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
+    url(r'^preguntas/$', views.preguntas),
+
 ]
